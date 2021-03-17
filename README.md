bcc289dd11159faf95fdea4573d77c3d3efa3490
kleinbottle4 <kleinbottle4@gmail.com>
2021-03-17 21:02:07 +0000

    added --help

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 NAME
 
-    csv-to-sc - convert text files into a .sc spreadsheets based on a delimiter
+	csv-to-sc - convert text files into .sc spreadsheets based on a
+	delimiter
 
 USAGE
 
@@ -8,7 +9,8 @@ USAGE
 
 DESCRIPTION
 
-csv-to-sc is a small program which converts a text file into a .sc file for use with the sc terminal spreadsheet editor.
+csv-to-sc is a small program which converts a text file into a .sc
+file for use with the sc terminal spreadsheet editor.
 
 USER OPTIONS (none of them is required):
 
@@ -33,14 +35,15 @@ USER OPTIONS (none of them is required):
     --copyright
         display copyright information
 
-    -s, --silent
-        do not display the little GNU GPL message at the beginning
-
 Do not write options together (e.g. -fd).
 
-If an option is not given, a prompt will ask for it (but see below about -p).
-If all the options are given, then no user prompts are required. This is useful for Bash scripts.
+If an option is not given, a prompt will ask for it (but see below
+about -p).
 
-If -p is given, then no prompt regarding writing to files will be given, except if -o has been specified while -m has not
+If all the options are given, then no user prompts are required. This
+is useful for Bash scripts.
+
+If -p is given, then no prompt regarding writing to files will be
+given, except if -o has been specified while -m has not
 
 If both -p and -o are given, -o takes priority.
--- main.py
+++ main.py
@@ -1,30 +1,30 @@
-#'main.py' is the main program for the csv-to-sc program.
-#It  converts .csv files into .sc files.
+# 'main.py' is the main program for the csv-to-sc program.
+# It  converts .csv files into .sc files.
 
 GPL_COPYRIGHT = '''
-#Copyright (C) 2019  syed343 (GitHub)
-
-#This program is free software: you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation, either version 3 of the License, or
-#(at your option) any later version.'''
+# Copyright (C) 2019  kleinbottle4 (GitHub)
+  
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.'''
 
 GPL_WARRANTY = '''
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.'''
-
-#You should have received a copy of the GNU General Public License
-#along with this program.  If not, see https://www.gnu.org/licenses/
-#or https://github.com/syed343/csv-to-sc/blob/master/LICENSE/.'''
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.'''
+  
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see https://www.gnu.org/licenses/
+# or https://github.com/kleinbottle4/csv-to-sc/blob/master/LICENSE/.'''
 
 from parse_strings import is_subset, is_int, is_float
 from parse_strings import table_to_nums, num_to_sc_col
 from sc_table import SC_table
 
 def main(args):
-    show_gpl(args)
+    show_help(args)
     delim = get_delimiter(args)
     table = SC_table(get_csv(args, delim))
     formats = table.get_cell_formats()
@@ -35,47 +35,48 @@ def main(args):
     output_list(sc_list, filename, mode)
     return 0
 
-def show_gpl(args):
-    if args.count('--silent') == 0:
-        if args.count('--copyright') >= 1:
-            print(GPL_COPYRIGHT)
-        if args.count('--warranty') >= 1:
-            print(GPL_WARRANTY)
+def show_help(args):
+    if args.count('--help') >= 1:
+        print("See https://github.com/kleinbottle4/csv-to-sc/blob/master/README.md.")
+    if args.count('--copyright') >= 1:
+        print(GPL_COPYRIGHT)
+    if args.count('--warranty') >= 1:
+        print(GPL_WARRANTY)
 
 def next_arg(args, opt):
     return args[args.index(opt) + 1]
 
 def get_delimiter(args):
     if args.count('-d') == 0:
-        delim = input('#Delimiter: ')
+        delim = input('# Delimiter: ')
     else:
         delim = next_arg(args, '-d')
     return delim
 
 def get_csv(args, delim):
     if args.count('-f') == 0:
-        #-f not given
-        filename = input('#CSV file: ')
+        # -f not given
+        filename = input('# CSV file: ')
     else:
-        #-f given
+        # -f given
         filename = next_arg(args, '-f')
     valid = False
     while valid == False:
         try:
             file = open(filename)
         except FileNotFoundError:
-            print('#File not found. Try again...')
-            filename = input('#CSV file: ')
+            print('# File not found. Try again...')
+            filename = input('# CSV file: ')
         else:
             valid = True
     text = file.read()
     text = text.replace('"', r'\"')
     file.close()
-    #remove extra newline at the end
+    # remove extra newline at the end
     if text[-1] == '\n':
         text = text[:-1]
     raw = text.split('\n')
-    #return a 2d array
+    # return a 2d array
     return [i.split(delim) for i in raw]
 
 def generate_sc_list(table):
@@ -99,29 +100,29 @@ def generate_sc_list(table):
 
 def get_filename(args):
     if args.count('-o') == 0:
-        #-o not given
+        # -o not given
         if args.count('-p') == 0:
-            #-p not given
-            if input('#Write output to file? (y/N) ').lower().startswith('y'):
-                filename = input('#Filename: (c = cancel) ')
+            # -p not given
+            if input('# Write output to file? (y/N) ').lower().startswith('y'):
+                filename = input('# Filename: (c = cancel) ')
             else:
                 filename = 'c'
         else:
-            #-p given
+            # -p given
             filename = 'c'
     else:
-        #-o given
+        # -o given
         filename = next_arg(args, '-o')
     return filename
 
 def get_write_mode(args, filename):
-    #filename 'c' means cancel
+    # filename 'c' means cancel
     if filename != 'c':
         if args.count('-m') == 0:
-            mode = input('#write (w) append (a) cancel (c) ').lower()
+            mode = input('# write (w) append (a) cancel (c) ').lower()
         else:
             mode = next_arg(args, '-m')
-        #make sure mode is valid (defaults to 'c')
+        # make sure mode is valid (defaults to 'c')
         if mode.startswith('w'):
             mode = 'w'
         elif mode.startswith('a'):
@@ -129,7 +130,7 @@ def get_write_mode(args, filename):
         else:
             mode = 'c'
     else:
-        #if filename is 'c'ancel
+        # if filename is 'c'ancel
         mode = 'c'
     return mode
 
